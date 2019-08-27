3eeda8e66d52a14be1d0145f10db57096e906349
Andrei Leonard Nicusan <a.l.nicusan@bham.ac.uk>
2019-08-27 11:56:30 +0100

    excluded cache files

--- setup.py
+++ setup.py
@@ -149,7 +149,7 @@ setup(
     install_requires=REQUIRED,
     extras_require=EXTRAS,
     include_package_data=True,
-    keywords='pept',
+    keywords='pept positron emission particle tracking',
     license='GNU',
     classifiers=[
         # Trove classifiers
