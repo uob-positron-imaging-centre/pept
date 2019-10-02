4fd62858e95943a25a22ddfe7bdb9ad66a933afa
Andrei Leonard Nicusan <a.l.nicusan@bham.ac.uk>
2019-10-02 11:16:47 +0100

    initial simulator

--- pept/simulation/__init__.py
+++ pept/simulation/__init__.py
@@ -1,10 +1,32 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+
 # File   : __init__.py
 # License: License: GNU v3.0
 # Author : Andrei Leonard Nicusan <a.l.nicusan@bham.ac.uk>
-# Date   : 27.08.2019
+# Date   : 21.08.2019
 
+
+from    .peptsim    import  Noise
+from    .peptsim    import  Shape
+from    .peptsim    import  Simulator
+
+
+__all__ = [
+    'Noise',
+    'Shape',
+    'Simulator'
+]
+
+
+__author__ =        "Andrei Leonard Nicusan"
+__credits__ =       ["Andrei Leonard Nicusan", "Kit Windows-Yule", "Sam Manger"]
+__license__ =       "GNU v3.0"
+__version__ =       "0.1"
+__maintainer__ =    "Andrei Leonard Nicusan"
+__email__ =         "a.l.nicusan@bham.ac.uk"
+__status__ =        "Development"
 
 
 
